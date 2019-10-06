61d77c017cc6ad39501c7f54eac34237f27d3e13
shozawa <tomohiro.shozawa@gmail.com>
2019-10-06 15:11:43 +0900

    section 009

--- src/Main.elm
+++ src/Main.elm
@@ -1,4 +1,15 @@
-module Main exposing (add, output)
+module Main exposing (Language(..), User(..), add, greet, greetInJa, message, output)
+
+
+type Language
+    = En
+    | Ja
+    | Fr
+
+
+type User
+    = LoggedIn Bool String
+    | Guest
 
 
 output : String
@@ -9,3 +20,33 @@ output =
 add : number -> number -> number
 add a b =
     a + b
+
+
+greet : Language -> String
+greet lang =
+    case lang of
+        En ->
+            "hello"
+
+        Ja ->
+            "こんにちは"
+
+        Fr ->
+            "Bonjour"
+
+
+greetInJa =
+    greet Ja
+
+
+message : User -> String
+message user =
+    case user of
+        LoggedIn True name ->
+            "こんにちは、" ++ name ++ "（管理者）さん"
+
+        LoggedIn False name ->
+            "こんにちは、" ++ name ++ "さん"
+
+        Guest ->
+            "こんちは、ゲストさん"
